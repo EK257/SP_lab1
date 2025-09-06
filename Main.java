f7bdbf81da40b5adc59a1319743e1b6cc50275be
EK257 <klime777@ya.ru>
2025-09-06 23:52:28 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.print("Введите b: ");
         sc = new Scanner(System.in);
         int b = sc.nextInt();
-        System.out.println(divideAndConquerMultiply(a,b));
+        System.out.println(secondMultiply(a,b));
         System.out.println(simpleMultiply(a,b));
         System.out.println(fastMultiply(a,b));
         System.out.println(bigIntMultiply(String.valueOf(a),String.valueOf(b)));
@@ -20,10 +20,16 @@ public class Main {
         BigInteger b = new BigInteger(bStr);
         return a.multiply(b).toString();
     }
-    public static long divideAndConquerMultiply(long a, long b) {
-        if (b == 0) return 0;
-        if (b % 2 == 0) return divideAndConquerMultiply((a << 1), (b >> 1));
-        return a + divideAndConquerMultiply(a, b - 1);
+    public static int secondMultiply(int a, int b) {
+        if (a == 0 || b == 0) return 0;
+        boolean negative = (a < 0) ^ (b < 0);
+        int x = a < 0 ? -a : a;
+        int y = b < 0 ? -b : b;
+        int result = 0;
+        for (int i = 0; i < y; i++) {
+            result += x;
+        }
+        return negative ? -result : result;
     }
     public static long simpleMultiply(long a, long b) {
         if (b == 0) return 0;
@@ -31,14 +37,20 @@ public class Main {
         return a + simpleMultiply(a, b - 1);
     }
     public static long fastMultiply(long a, long b) {
-        long result = 0L;
+        boolean isNegative = (a < 0) ^ (b < 0);
+        a = Math.abs(a);
+        b = Math.abs(b);
+        long result = 0;
+
         while (b > 0) {
-            if ((b & 1) == 1) {
-                result += a;
-            }
+            if ((b & 1) == 1) result += a;
             a <<= 1;
-            b >>>= 1;
+            b >>= 1;
         }
-        return result;
+        if (isNegative) {
+            return -result;
+        } else {
+            return result;
+        }
     }
 }
